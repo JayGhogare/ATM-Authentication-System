69a6dc1901d313b7c34ef665bcec09fef7747b8e
Jay Ghogare <ghogarejay01@gmail.com>
2026-08-11 20:41:06 +0530

    Fix syntax error in account lock message

--- ATM_PIN_Checker.py
+++ ATM_PIN_Checker.py
@@ -10,4 +10,4 @@ for i in range(3):
         print("Wrong Pin (Attempt", i + 1, "/3)")
 
 if Password != Pin:
-    print("Account is Locked after 3 Attempts"))
+    print("Account is Locked after 3 Attempts")
